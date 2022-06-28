498ea8f93121959803b1eb3c7eb567959c54ff47
kimbugs <kimbugs9984@gmail.com>
2022-06-28 10:06:34 +0900

    test-sasl done

--- test-ssl/kafka-test-consumer.py
+++ test-ssl/kafka-test-consumer.py
@@ -1,7 +1,7 @@
 import ssl
 from kafka import KafkaConsumer, KafkaProducer
 
-kafka_brokers='localhost:19092,localhost:29092,localhost:39092'
+kafka_brokers='localhost:19093,localhost:29093,localhost:39093'
 ca_root_location='CARoot.pem'
 cert_location='certificate.pem'
 key_location='key.pem'
@@ -14,9 +14,12 @@ password='secret'
 consumer = KafkaConsumer(
     topic,
     bootstrap_servers=kafka_brokers,
-    group_id='consumer-id',
+    #group_id='consumer-id',
     #ssl_context=context,
-    security_protocol='SSL',
+    security_protocol='SASL_SSL',
+    sasl_mechanism="SCRAM-SHA-256",
+    sasl_plain_username='test',
+    sasl_plain_password='test',
     ssl_check_hostname=False,
     ssl_cafile=ca_root_location,
     ssl_certfile=cert_location,
